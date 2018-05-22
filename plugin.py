bf9553700b38d0bf32c2af6366762e812d7520a3
omerxx <omerxx@gmail.com>
2018-05-22 17:59:29 +0300

    Adding status check functionality

--- plugin.py
+++ plugin.py
@@ -1,5 +1,6 @@
 import os
 import boto3
+import time
 
 def pp(name):
     # pp as in plugin parameter 
@@ -42,10 +43,32 @@ def update_stack(client):
     print response
 
 
+def stack_status(client):
+    start = time.time()
+    timeout = 180 if not pp('timeout') else pp('timeout')
+    response = client.describe_stacks(StackName=pp('stackname'))
+    status = response["Stacks"][0]["StackStatus"]
+    
+    while "COMPLETE" not in status and time.time()-start < timeout:
+        print '[{}]: {}'.format(time.time(), status)
+        time.sleep(5)
+        response = client.describe_stacks(StackName=pp('stackname'))
+        status = response["Stacks"][0]["StackStatus"]
+    elif status == "UPDATE_IN_PROGRESS":
+        response = client.cancel_update_stack(StackName=pp('stackname'))
+        print "Canceling update"
+    else:
+        print 'Check stack {}. Status is {}'.format(pp('stackname'), status)
+        exit(1)
+
+
+
 if __name__ == "__main__":
     # os.environ['AWS_DEFAULT_REGION'] = 'us-east-1' if not pp('region') else pp('region')
 
     client = boto3.client('cloudformation', region_name='us-east-1')
     update_stack(client)
+    time.sleep(5)
+    stack_status(client)
 
  
