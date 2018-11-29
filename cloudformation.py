51606f74c8e8bf43a01380be4564c846d5dfe5e9
omerxx <omerxx@gmail.com>
2018-11-29 17:28:29 +0200

    Use stackname from function param in get_stack_parameters

--- cloudformation.py
+++ cloudformation.py
@@ -4,7 +4,7 @@ from plugin import pp
 parameters = {}
 
 def get_stack_parameters(client, stackName):
-    response = client.describe_stacks(StackName=stackname)
+    response = client.describe_stacks(StackName=stackName)
     for param in response["Stacks"][0]["Parameters"]:
         parameters[param["ParameterKey"]] = param["ParameterValue"]
 
